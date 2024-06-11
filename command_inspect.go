a4e83e990b469f45939bd40eab5bc724d7e61e52
ahgr3y <leehh97@gmail.com>
2024-06-11 15:51:18 +0800

    Added inspect pokemon

--- command_inspect.go
+++ command_inspect.go
@@ -1,12 +1,36 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 func commandInspect(cfg *config, args ...string) error {
 
 	// Command should have one pokemon as argument
 	if len(args) != 1 {
 		return errors.New("please select one pokemon to inspect")
+	}
+
+	pokemon := args[0]
+
+	// Cannot inspect a pokemon that has not been caught
+	pokemonSpecs, exist := cfg.pokeCache.Get(pokemon)
+	if !exist {
+		return errors.New("unable to inspect pokemon that has not been caught yet")
+	}
+
+	// Print out details of pokemon
+	fmt.Printf("Name: %v\n", pokemonSpecs.Name)
+	fmt.Printf("Height: %v\n", pokemonSpecs.Height)
+	fmt.Printf("Weight: %v\n", pokemonSpecs.Weight)
+	fmt.Println("Stats:")
+	for _, stat := range pokemonSpecs.Stats {
+		fmt.Printf("    - %v: %v\n", stat.Stat.Name, stat.BaseStat)
+	}
+	fmt.Println("Types:")
+	for _, t := range pokemonSpecs.Types {
+		fmt.Printf("    - %v\n", t.Type.Name)
 	}
 	return nil
 }
